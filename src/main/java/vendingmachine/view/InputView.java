42c7b2d41042b8d8ddabd48c83b0b86a7ace0d3a
Seed <hsik0225@gmail.com>
2021-08-09 02:37:57 +0900

    feat: InputView 및 OutputView 구현

---
+++ src/main/java/vendingmachine/view/InputView.java
@@ -0,0 +1,67 @@
+package vendingmachine.view;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import com.woowahan.techcourse.utils.Scanners;
+
+import vendingmachine.model.Money;
+import vendingmachine.model.Product;
+import vendingmachine.model.Products;
+
+public class InputView {
+
+    public static final int PRODUCT_NAME_INDEX = 0;
+    public static final int PRODUCT_AMOUNT_INDEX = 1;
+    public static final int PRODUCT_PRICE_INDEX = 2;
+
+    public static final int BRACKET_BEGIN_INDEX = 1;
+    public static final Pattern PRODUCT_PATTERN = Pattern.compile("((\\[[^\\]]+\\]),?)+?");
+
+    public static Money askHoldingMoney() {
+        return Money.from(Integer.parseInt(input("자판기가 보유하고 있는 금액을 입력해 주세요.")));
+    }
+
+    private static String input(String message) {
+        System.out.println(message);
+        final String input = Scanners.nextLine();
+        System.out.println();
+        return input;
+    }
+
+    public static Products askProducts() {
+        return Products.from(convertInputToListOfProduct());
+    }
+
+    private static List<Product> convertInputToListOfProduct() {
+        Matcher matcher = PRODUCT_PATTERN.matcher(input("상품명과 수량, 금액을 입력해 주세요.").trim());
+
+        List<String> products = new ArrayList<>();
+        while (matcher.find()) {
+            products.add(matcher.group(2));
+        }
+
+        return products.stream()
+                       .map(InputView::removeBrackets)
+                       .map(product -> {
+                           final String[] productInfo = product.split(",");
+                           return new Product(productInfo[PRODUCT_NAME_INDEX], productInfo[PRODUCT_AMOUNT_INDEX], productInfo[PRODUCT_PRICE_INDEX]);
+                       })
+                       .collect(Collectors.toList());
+    }
+
+    private static String removeBrackets(String product) {
+        return product.substring(BRACKET_BEGIN_INDEX, product.length() - 1);
+    }
+
+    public static Money askInputMoney() {
+        return Money.from(Integer.parseInt(input("투입할 금액을 입력해 주세요.")));
+    }
+
+    public static String askProductToBuy() {
+        return input("구매할 상품명을 입력해 주세요.");
+    }
+}
---
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -0,0 +1,20 @@
+package vendingmachine.view;
+
+import java.util.Map;
+
+public class OutputView {
+
+    public static void printRemainingMoney(int holdingMoney) {
+        System.out.printf("투입된 금액: %d원\n", holdingMoney);
+    }
+
+    public static void printCoinCount(Map<Integer, Integer> coinCount) {
+        System.out.println("잔돈");
+
+        coinCount.entrySet()
+                 .stream()
+                 .filter(entry -> entry.getValue() != 0)
+                 .sorted((c1, c2) -> c2.getKey().compareTo(c1.getKey()))
+                 .forEach(entry -> System.out.printf("%d원 - %d 개\n", entry.getKey(), entry.getValue()));
+    }
+}
